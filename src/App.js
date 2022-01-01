8a8617577ec99c1ad69c3471eb38869612263556
vish-han <vshlchauhab@gmail.com>
2022-01-01 20:53:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
+import React from 'react'
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[people,setpeople]=React.useState[data]
+ return <React.Fragment>
+   <section className='container'>
+<h2>
+ 5 Birthdays Today
+</h2>
+<div className='list'>
+  <div className='item'>
+    <h3>Vishal Chauhan</h3>
+    <p>19 years</p>
+  </div>
+  <div className='item'>
+    <h3>Rahul Chauhan</h3>
+    <p>
+      20 years
+    </p>
+    </div> 
+      <div className='item'>
+    <h3>Rahul Chauhan</h3>
+    <p>
+      20 years
+    </p>
+    </div> 
+     <div className='item'>
+    <h3>Rahul Chauhan</h3>
+    <p>
+      20 years
+    </p>
+    </div> 
+     <div className='item'>
+    <h3>Rahul Chauhan</h3>
+    <p>
+      20 years
+    </p>
     </div>
-  );
+</div>
+<button className='btn'>Click Me</button>
+ </section>
+ </React.Fragment>
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const data=[
+ {   name:'vishal' ,bday:19},
+ {
+     name:'rahul',bday:20
+ },
+   { name:'om' ,bday:19},
+     { name:'prateek' ,bday:19},
+]
